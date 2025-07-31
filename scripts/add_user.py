b2bfa236b028af4a1e9b6174523ec82d51b55e3d
Nkateko Tibane <nkatekotibane101@gmail.com>
2025-07-31 14:25:55 +0200

    Script to add users

---
+++ scripts/add_user.py
@@ -0,0 +1,32 @@
+import pymysql, bcrypt
+
+SERVER = "localhost"
+USER = "core"
+PASSWORD = "core"
+DATABASE = "c2_database"
+
+
+
+
+def AddUsers(username, password):
+    connection = pymysql.connect(host=SERVER, 
+                                user=USER, 
+                                password=PASSWORD, 
+                                database=DATABASE)
+    cur = connection.cursor()
+
+
+    hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+    cur.execute("INSERT INTO Operators (username, password) VALUES (%s, %s)", 
+                (username, hashed_pw))
+    
+    connection.commit()
+
+    cur.close()
+    connection.close()
+
+
+AddUsers("dev", "dev")
+
+
